57c4506523b252f4d031b8abc3c56f7411b8bdee
Noseh Taghaymurodzoda <nosehtagaymurodzoda@gmail.com>
2025-10-17 16:10:24 +0500

    Refactor Swagger setup in Program.cs for improved readability and maintainability.

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -134,12 +134,12 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Market API v1");
-    c.RoutePrefix = string.Empty;
-})
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Market API v1");
+        c.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseCors("AllowAll");
