9357e9d6e45b3aaf5c20a06e4f4c46a9f93f0c30
Noseh Taghaymurodzoda <nosehtagaymurodzoda@gmail.com>
2025-10-17 16:07:27 +0500

    Enhance Swagger configuration in Program.cs to set the API endpoint and route prefix for better documentation access.

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -132,12 +132,14 @@ builder.Services.AddCors(options =>
 
 
 var app = builder.Build();
-
-
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Market API v1");
+    c.RoutePrefix = string.Empty;
+})
 }
 
 app.UseCors("AllowAll");
